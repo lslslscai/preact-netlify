6872755cdd1e23c518fdacaa46a0b82234703775
lslslscai <2052366734@qq.com>
2020-04-25 12:59:48 +0800

    master

--- src/components/header/index.js
+++ src/components/header/index.js
@@ -3,13 +3,15 @@ import { Link } from 'preact-router/match';
 import style from './style';
 
 const Header = () => (
-	<header class={style.header}>
-		<Link href="/"><h1>Jane Doe</h1></Link>
-		<nav>
-			<Link activeClassName={style.active} href="/blogs">Blogs</Link>
-			<Link activeClassName={style.active} href="/contact">Contact me</Link>
-		</nav>
-	</header>
+	<div class={style.fixtop}>
+		<header class={style.header}>
+			<Link href="/"><img src ="/assets/logo+white.png"/></Link>
+			<nav>
+				<Link activeClassName={style.active} href="/blogs">Blogs</Link>
+				<Link activeClassName={style.active} href="/contact">Contact me</Link>
+			</nav>
+		</header>
+	</div>
 );
 
 export default Header;
